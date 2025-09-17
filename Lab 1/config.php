98f3b90cd5bdcd4a9ac64f7931d0631a8624463f
Oluseyi21 <oluseyisosanya@gmail.com>
2025-09-17 14:43:46 -0400

    Update config.php

--- Lab 1/config.php
+++ Lab 1/config.php
@@ -1,7 +1,8 @@
 <?php
 //News Api key
-define("News_API_KEY", "aa5a276be41a449d83572ab1620eef40");
+define("News_API_KEY", "YOUR_NEWS_API_KEY_HERE");
 
 //Base URL for News
 define("News_BASE_URL", "https://newsapi.org");
+
 ?>
