372b67c33fdd4cc4397fe24cbb0d96047d6fb438
Oluseyi21 <oluseyisosanya@gmail.com>
2025-09-30 13:59:32 -0400

    Delete Assignment 2 directory

--- Assignment 2/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-    define("DB_HOST", "172.31.22.43");
-    define("DB_NAME", "Oluseyi200616929");
-    define("DB_USER", "Oluseyi200616929");
-    define("DB_PASS", "0GppqdnJwS");
-?>
--- Assignment 2/templates/footer.php
+++
@@ -1,7 +0,0 @@
-</main>
-
-<footer>
-    <p>&copy; <?php echo date("Y"); ?> Pizza Shop. All rights reserved</p>
-</footer>
-</body>
-</html>
