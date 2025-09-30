6e00e1c90dfa6d64e856ef7b5c86c4fccb7ac74d
Oluseyi21 <oluseyisosanya@gmail.com>
2025-09-30 13:49:29 -0400

    Add files via upload

---
+++ Assignment 2/templates/footer.php
@@ -0,0 +1,7 @@
+</main>
+
+<footer>
+    <p>&copy; <?php echo date("Y"); ?> Pizza Shop. All rights reserved</p>
+</footer>
+</body>
+</html>
---
+++ Assignment 2/templates/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Pizza order Form">
+    <title>Pizza Order Form</title>
+    <link rel="stylesheet" href="./CSS/style.css">
+</head>
+<body>
+<header>
+    <h1>Welcome to Pizza Shop</h1>
+    <p>Place your order below</p>
+</header>
+<main>
